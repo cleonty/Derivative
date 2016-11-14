5875809f64a6b7a2805d8b3fee82f4005ccdcd7c
cleonty <leonty@inbox.ru>
2016-11-14 13:44:50 +0500

    One more example

--- src/derivative/Main.java
+++ src/derivative/Main.java
@@ -2,7 +2,18 @@ package derivative;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void test1() {
+    Variable x = new Variable("x");
+    Constant five = new Constant(5);
+    Constant nine = new Constant(9);
+    
+    Derivable five_x = new Mul(five, x);
+    Derivable five_x_plus_nine = new Sum(five_x, nine);
+    
+    System.out.println("d(" + five_x_plus_nine + ")/d" + x + " = " + five_x_plus_nine.derive(x));
+  }
+  
+  public static void test2() {
     Variable x = new Variable("x");
     Variable y = new Variable("y");
     Constant five = new Constant(5);
@@ -15,6 +26,11 @@ public class Main {
     
     Derivable d = new Mul(five_x_plus_nine_y, five_x_plus_nine_y);
     System.out.println("d(" + d + ")/d" + x + " = " + d.derive(x));
+  }
+  
+  public static void main(String[] args) {
+    test1();
+    test2();
   }
 
 }
